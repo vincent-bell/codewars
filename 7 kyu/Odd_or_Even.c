242af50ed3044b9678ee7c7a460677f69126d45c
vincent-bell <147046360+vincent-bell@users.noreply.github.com>
2023-10-15 14:44:43 +0100

    Refactored Odd_or_Even 🎨

--- 7 kyu/Odd_or_Even.c
+++ 7 kyu/Odd_or_Even.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 
-
 const char *odd_or_even(const int *arr, size_t n)
 {
     int sum = 0;
 
     for (size_t i = 0; i < n; i++) sum += arr[i];
-
 
     return sum % 2 == 0 ? "even" : "odd";
 }
