7c24acd73513f90e1475f2e77342650dc5b31ce0
vincent-bell <147046360+vincent-bell@users.noreply.github.com>
2023-11-11 22:28:48 +0000

    Solved Multiplication table 📝

--- 6 kyu/Multiplication table.c
+++ 6 kyu/Multiplication table.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 
 int **multiplication_table(int n) {
-	int **matrix=(int **) malloc (n*sizeof(int *));
+	int **matrix = (int**) malloc(n*sizeof(int *));
 
 	for(int i=0;i<n;i++){
-		matrix[i]=(int *) malloc (n*sizeof(int));
+		matrix[i] = (int*) malloc(n*sizeof(int));
 		for(int j=0;j<n;j++)
-			matrix[i][j]=(i+1)*(j+1);
+			matrix[i][j] = (i+1)*(j+1);
 	}
 
 	return matrix;
